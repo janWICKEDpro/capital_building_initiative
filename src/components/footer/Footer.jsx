9948c1dea30883a24222ef0fed7934a34064164c
janWICKEDpro <janroyal45@gmail.com>
2022-06-20 02:53:09 +0100

    removed rubbish

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@
   export default function Footer() {
     
     return (
-      <div className="footer">
+      <div className="footer" id='footer'>
 
        <div className="container">
          <div className="row">
@@ -15,30 +15,30 @@
                <li><a href="#intro">Home</a></li>
                <li><a href="#businessinfo">about us</a></li>
                <li><a href="#service">our services</a></li>
-               <li><a href="#">privacy policy</a></li>
+               <li><a href="#footer">privacy policy</a></li>
              </ul>
            </div>
            <div className="footer-col">
              <h4>Location</h4>
              <ul>
-               <li><a href="#">Buea</a></li>
+               <li><a href="https://en.wikipedia.org/wiki/Buea">Buea</a></li>
              </ul>
            </div>
            <div className="footer-col">
              <h4>Contacts</h4>
              <ul>
-               <li><a href="#">673644734</a></li>
-               <li><a href="#">673218313</a></li>
-               <li><a href="#">650871913</a></li>
-               <li><a href="#">620013095</a></li>
+               <li>673644734</li>
+               <li>673218313</li>
+               <li>650871913</li>
+               <li>620013095</li>
              </ul>
            </div>
            <div className="footer-col">
              <h4>follow us</h4>
              <div className="social-links">
-               <a href="#"><Facebook /></a>
+               <a href="https://www.facebook.com/help/821153694683665"><Facebook /></a>
                <a href="https://twitter.com/JanRoyal12"><Twitter /></a>
-               <a href="#"><Instagram /></a>
+               <a href="instagram.com"><Instagram /></a>
              
              </div>
            </div>
