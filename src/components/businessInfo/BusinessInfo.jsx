e3f143a3d5e82e068ff4a5398b6f080f0c94ac6c
janWICKEDpro <janroyal45@gmail.com>
2022-08-06 00:17:54 +0100

    ok

--- src/components/businessInfo/BusinessInfo.jsx
+++ src/components/businessInfo/BusinessInfo.jsx
@@ -28,7 +28,7 @@ export default function BusinessInfo() {
   ];
   return (
     <div className="businessinfo" id="businessinfo">
-      <h2>Managers</h2>
+      <h1>Managers</h1>
       <div className="container">
         {data.map((d) => (
           <div key={d.id} className={d.featured ? "card featured" : "card"}>
